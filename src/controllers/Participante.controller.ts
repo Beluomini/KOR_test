de2b569c015865497351661c07ede87f0a7e1cf7
Beluomini <lucasbeluomini1@gmail.com>
2024-04-18 19:50:10 -0300

    Criação do CRUD de Participante completo, mas sem verificação de erros

--- src/controllers/Participante.controller.ts
+++ src/controllers/Participante.controller.ts
@@ -4,6 +4,17 @@ import { ParticipanteDTO } from "../dto/Participante.dto";
 
 export class ParticipanteController {
 
+    static async getAllParticipantes(req: Request, res: Response) {
+        const participantes = await AppDataSource.getRepository("Participante").find();
+        res.send(participantes);
+    }
+
+    static async getParticipanteById(req: Request, res: Response) {
+        const { id } = req.params;
+        const participante = await AppDataSource.getRepository("Participante").findOneBy({"id": id});
+        res.send(participante);
+    }
+
     static async createParticipante(req: Request, res: Response) {
         const newParticipante:ParticipanteDTO = {
             nome: req.body.nome,
@@ -16,6 +27,27 @@ export class ParticipanteController {
         const database = await AppDataSource.getRepository("Participante").save(newParticipante);
 
         res.send(newParticipante);
+    }
+
+    static async updateParticipante(req: Request, res: Response) {
+        const { id } = req.params;
+        const newParticipante:ParticipanteDTO = {
+            nome: req.body.nome,
+            email: req.body.email,
+            documento: req.body.documento,
+            tipo: req.body.tipo,
+            processos: req.body.processos
+        }
+
+        const database = await AppDataSource.getRepository("Participante").update(id, newParticipante);
+
+        res.send(newParticipante);
+    }
+
+    static async deleteParticipante(req: Request, res: Response) {
+        const { id } = req.params;
+        const database = await AppDataSource.getRepository("Participante").delete(id);
+        res.send("Participante deletado com sucesso!");
     }
 
 }
--- src/routes/Participante.routes.ts
+++ src/routes/Participante.routes.ts
@@ -3,8 +3,24 @@ import { ParticipanteController } from '../controllers/Participante.controller';
 
 const Router = express.Router();
 
+Router.get('/', (req, res) => {
+    ParticipanteController.getAllParticipantes(req, res);
+});
+
+Router.get('/:id', (req, res) => {
+    ParticipanteController.getParticipanteById(req, res);
+});
+
 Router.post('/', (req, res) => {
     ParticipanteController.createParticipante(req, res);
+});
+
+Router.put('/:id', (req, res) => {
+    ParticipanteController.updateParticipante(req, res);
+});
+
+Router.delete('/:id', (req, res) => {
+    ParticipanteController.deleteParticipante(req, res);
 });
 
 export { Router as particioanteRouter };
