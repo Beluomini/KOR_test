089282074f6fcb2ad1060497e2766a0a14cfa987
Beluomini <lucasbeluomini1@gmail.com>
2024-04-18 21:29:45 -0300

    rotas concluidas, mas sem a lógica pronta

--- src/routes/Processo.routes.ts
+++ src/routes/Processo.routes.ts
@@ -3,12 +3,20 @@ import { ProcessoController } from '../controllers/Processo.controller';
 
 const Router = express.Router();
 
-Router.get('/', (req, res) => {
+Router.get('/todos', (req, res) => {
     ProcessoController.getAllProcessos(req, res);
 });
 
 Router.get('/:id', (req, res) => {
     ProcessoController.getProcessoById(req, res);
+});
+
+Router.get('/', (req, res) => {
+    ProcessoController.getProcessoByClienteAndParticipante(req, res);
+});
+
+Router.get('/:id/participantes', (req, res) => {
+    ProcessoController.getProcessoParticipantes(req, res);
 });
 
 Router.post('/', (req, res) => {
