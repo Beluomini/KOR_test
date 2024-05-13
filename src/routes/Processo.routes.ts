e56608c34d12ef486f96398fb56bce9c0771ad17
Beluomini <lucasbeluomini1@gmail.com>
2024-05-13 15:24:59 -0300

    processo get all fixed

--- src/routes/Processo.routes.ts
+++ src/routes/Processo.routes.ts
@@ -3,6 +3,10 @@ import { ProcessoController } from '../controllers/Processo.controller';
 
 const Router = express.Router();
 
+Router.get('/todos', (req, res) => {
+    ProcessoController.getAllProcessos(req, res);
+});
+
 Router.get('/', (req, res) => {
     ProcessoController.getProcessoByClienteAndParticipante(req, res);
 });
@@ -15,9 +19,6 @@ Router.get('/:id/participantes', (req, res) => {
     ProcessoController.getProcessoParticipantes(req, res);
 });
 
-Router.get('/todos', (req, res) => {
-    ProcessoController.getAllProcessos(req, res);
-});
 
 Router.post('/', (req, res) => {
     ProcessoController.createProcesso(req, res);
